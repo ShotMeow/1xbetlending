38079776c5963983771fafd3397db898ee41a04b
ShotMeow <shotmeow@gmail.com>
2024-04-06 18:35:30 +0300

    Fixed: Improve SEO, Improve Code

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -2,6 +2,7 @@ import type { FC } from "react";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Switch from "@/src/features/switch-language/ui/Switch";
+import Link from "next/link";
 
 const Header: FC = () => {
   const translate = useTranslations("header");
@@ -9,7 +10,9 @@ const Header: FC = () => {
   return (
     <header className="lg:fixed lg:w-full z-50 bg-ocean-dark my-8 py-8">
       <div className="container mx-auto flex lg:flex-row lg:gap-0 gap-10 flex-col items-center justify-between">
-        <Image src="/logo.png" alt="1XBET" width={166} height={32} />
+        <a href="#primary">
+          <Image src="/logo.png" alt="1XBET" width={166} height={32} />
+        </a>
         <nav>
           <ul className="flex items-center gap-10 lg:gap-16 lg:flex-row flex-col font-light text-[22px] uppercase">
             <li>
