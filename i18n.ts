a74f96ac4f4bf230a970eafcfcd7f499916be338
ShotMeow <shotmeow@gmail.com>
2024-04-23 14:08:18 +0300

    Fixed: get arabic flag

--- i18n.ts
+++ i18n.ts
@@ -1,9 +1,11 @@
 import { getRequestConfig } from "next-intl/server";
+import { notFound } from "next/navigation";
 
 export const locales = ["en", "fr", "ar"];
-export const localePrefix = "as-needed";
 
 export default getRequestConfig(async ({ locale }) => {
+  if (!locales.includes(locale as any)) notFound();
+
   return {
     messages: (await import(`./messages/${locale}.json`)).default,
   };
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,8 @@
 import createMiddleware from "next-intl/middleware";
-import { localePrefix, locales } from "./i18n";
+import { locales } from "./i18n";
 
 export default createMiddleware({
   locales,
-  localePrefix,
   defaultLocale: "en",
 });
 
