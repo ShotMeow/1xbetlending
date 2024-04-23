a9a2241fc17196a18759fa0327f9ad976db3cfba
ShotMeow <shotmeow@gmail.com>
2024-04-23 14:09:07 +0300

    Fixed: get arabic flag

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,7 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  i18n: {
-    locales: ['en', 'fr', 'ar'],
-    defaultLocale: 'en',
-    domains: [
-      {
-        domain: '1xbetlending-pink.vercel.app/en',
-        defaultLocale: 'en',
-      },
-      {
-        domain: '1xbetlending-pink.vercel.app/ar',
-        defaultLocale: 'ar',
-      },
-      {
-        domain: '1xbetlending-pink.vercel.app/fr',
-        defaultLocale: 'fr',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
 
